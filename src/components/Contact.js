3c3b737e2ba4c4643def476efcb9679737149716
Charlotte LECONTE <charlotte.leconte91@gmail.com>
2019-10-15 13:14:11 +0200

    last good version of contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,23 +1,7 @@
 import React from 'react';
 import './Contact.css';
 
-// const Contact = (props) => {
-//     return (
-//         <div className = "Contact">
-//             <img className = 'avatar' src = {props.avatar} alt = '{props.name}'/>
-//             <div className = 'status'>
-//                 <div className = 'name' >{props.name}</div>
-//                 <div className = 'status'>
-//                     <div class ='block'>
-//                         <div className = {props.online ? 'status-online': 'status-offline'}>
-//                         </div>
-//                         <p>{props.online ?  'Online': 'Offline' }</p>
-//                     </div>
-//                 </div>
-//             </div>
-//         </div>
-//     )
-// }
+
 
 class Contact extends React.Component {
     constructor(props) {
